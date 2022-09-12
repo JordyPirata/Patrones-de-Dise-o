7b2819508fb8f06a2099a9bced0db477007298f9
JordyPirata <103045128+JordyPirata@users.noreply.github.com>
2022-09-12 16:59:09 -0500

    Initial commit

---
+++ Assets/PowerUp.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace PowerUps
+{
+    public abstract class PowerUp : MonoBehaviour
+    {
+        [SerializeField] public string _id;
+        public string Id => _id;
+
+    }
+}
+
---
+++ Assets/PowerUpSpawner.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace PowerUps
+{
+    public class PowerUpSpawner : MonoBehaviour
+    {
+        [SerializeField] private PowerUpFactory _powerUpFactory;
+        private void Update()
+        {
+            if (Input.GetKey(KeyCode.Alpha1))
+            {
+                _powerUpFactory.Create("Speed");
+            }else if (Input.GetKey(KeyCode.Alpha2))
+            {
+                _powerUpFactory.Create("Drunk");
+            }
+        }
+    }
+}
